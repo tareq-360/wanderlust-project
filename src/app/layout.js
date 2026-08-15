bad7c010c06adb714fad8b9d5b6487a1e8d41352
tareq-360 <mdtareqhossain2002@gmail.com>
2026-08-16 00:56:18 +0600

    Navbar update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Foother from "@/components/Foother";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,9 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Navbar></Navbar>
         {children}
+        <Foother></Foother>
         </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Main Page </h1>
+      <Hero></Hero>
     </div>
   );
 }
