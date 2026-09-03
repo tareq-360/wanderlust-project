0b2acbda0f38c9c3230f0579c31ae37079d71c03
tareq-360 <mdtareqhossain2002@gmail.com>
2026-09-03 15:16:22 +0600

    social signup -google and github done

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -9,5 +9,15 @@ export const auth = betterAuth({
     emailAndPassword: { 
     enabled: true, 
   }, 
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.CLIENT_SECRET, 
+        }, 
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID , 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET, 
+        }, 
+    },
     database: mongodbAdapter(db),
 });
--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -5,9 +5,10 @@ import { Button, Description, FieldError, Form, Input, Label, TextField } from "
 import { authClient } from "../lib/auth-client";
 import { redirect } from "next/navigation";
 import { toast, ToastContainer } from "react-toastify";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 export default function Signup() {
-  const onSubmit = async(e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const userData = Object.fromEntries(formData.entries());
@@ -15,101 +16,118 @@ export default function Signup() {
     const { name, imageUrl, email, password } = userData;
     // console.log(name,imageUrl,email,password);
     const { data, error } = await authClient.signUp.email({
-      name: name, 
-      email: email, 
+      name: name,
+      email: email,
       password: "password1234",
-      image: imageUrl, 
-      callbackURL: "/", 
+      image: imageUrl,
+      callbackURL: "/",
     });
     // console.log(data);
-    if(data){
+    if (data) {
       redirect("/")
     }
-    else{
+    else {
       toast.error(error.message);
     }
-
-
-
-
   }
-  return (
-    <div className=" flex justify-center py-10 mx-auto container h-screen bg-slate-200">
-      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+  const signInWithGoogle = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+  };
 
-        <TextField
-          isRequired
-          name="name"
-          type="text"
+  const signInWithGithub = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github"
+    })
+  }
+    return (
+      <div className="py-10 mx-auto container h-screen bg-slate-200">
+        <div className=" flex justify-center ">
+          <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
 
-        >
-          <Label>Name</Label>
-          <Input placeholder="John doe" />
-          <FieldError />
-        </TextField>
-        <TextField
-          isRequired
-          name="imageUrl"
-          type="text"
+            <TextField
+              isRequired
+              name="name"
+              type="text"
 
-        >
-          <Label>Image URL</Label>
-          <Input placeholder="https://images.unsplash.com/photo-1624" />
-          <FieldError />
-        </TextField>
-        <TextField
-          isRequired
-          name="email"
-          type="email"
-          validate={(value) => {
-            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-              return "Please enter a valid email address";
-            }
+            >
+              <Label>Name</Label>
+              <Input placeholder="John doe" />
+              <FieldError />
+            </TextField>
+            <TextField
+              isRequired
+              name="imageUrl"
+              type="text"
 
-            return null;
-          }}
-        >
-          <Label>Email</Label>
-          <Input placeholder="john@example.com" />
-          <FieldError />
-        </TextField>
+            >
+              <Label>Image URL</Label>
+              <Input placeholder="https://images.unsplash.com/photo-1624" />
+              <FieldError />
+            </TextField>
+            <TextField
+              isRequired
+              name="email"
+              type="email"
+              validate={(value) => {
+                if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                  return "Please enter a valid email address";
+                }
 
-        <TextField
-          isRequired
-          minLength={8}
-          name="password"
-          type="password"
-          validate={(value) => {
-            if (value.length < 8) {
-              return "Password must be at least 8 characters";
-            }
-            if (!/[A-Z]/.test(value)) {
-              return "Password must contain at least one uppercase letter";
-            }
-            if (!/[0-9]/.test(value)) {
-              return "Password must contain at least one number";
-            }
+                return null;
+              }}
+            >
+              <Label>Email</Label>
+              <Input placeholder="john@example.com" />
+              <FieldError />
+            </TextField>
 
-            return null;
-          }}
-        >
-          <Label>Password</Label>
-          <Input placeholder="Enter your password" />
-          <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
-          <FieldError />
-        </TextField>
+            <TextField
+              isRequired
+              minLength={8}
+              name="password"
+              type="password"
+              validate={(value) => {
+                if (value.length < 8) {
+                  return "Password must be at least 8 characters";
+                }
+                if (!/[A-Z]/.test(value)) {
+                  return "Password must contain at least one uppercase letter";
+                }
+                if (!/[0-9]/.test(value)) {
+                  return "Password must contain at least one number";
+                }
 
-        <div className="flex gap-2">
-          <Button type="submit">
-            <Check />
-            Sign up
-          </Button>
-          <Button type="reset" variant="secondary">
-            Reset
-          </Button>
+                return null;
+              }}
+            >
+              <Label>Password</Label>
+              <Input placeholder="Enter your password" />
+              <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+              <FieldError />
+            </TextField>
+
+            <div className="flex gap-2">
+              <Button type="submit">
+                <Check />
+                Sign up
+              </Button>
+              <Button type="reset" variant="secondary">
+                Reset
+              </Button>
+            </div>
+          </Form>
+
         </div>
-      </Form>
-      <ToastContainer />
-    </div>
-  );
-}
+        <div className=" flex justify-center py-2">
+          <div className=" space-x-5">
+            <button onClick={signInWithGoogle} className="text-green-600 font-bold btn-outline btn "><FaGoogle></FaGoogle> Sign up with google</button>
+            <button onClick={signInWithGithub} className="text-green-600 font-bold btn-outline btn "><FaGithub></FaGithub>Sign up with github</button>
+          </div>
+        </div>
+        <ToastContainer />
+      </div>
+
+    );
+  }
